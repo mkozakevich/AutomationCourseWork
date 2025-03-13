fcaaa9c74709eed5e6789fa2c636347ba90eb573
Maksim Kozakevich <kotseee@yandex.ru>
2025-03-13 03:21:03 +0300

    feat: logs

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -5,10 +5,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
+@Log4j2
 public class MainPage {
+    private static final Logger log = LoggerFactory.getLogger(MainPage.class);
     WebDriver driver;
 
     public MainPage(WebDriver driver) {
@@ -21,17 +26,23 @@ public class MainPage {
     private static final By SIGN_UP_SUBMIT_BUTTON = By.cssSelector("button[onclick='register()']");
 
     public MainPage open() {
+        log.info("Open main page");
+
         driver.get("https://www.demoblaze.com/");
         return this;
     }
 
     public MainPage openSignUpModal() {
+        log.info("Открытие модального окна регистрации");
+
         driver.findElement(SIGN_UP_NAV_BUTTON).click();
 
         return this;
     }
 
     public MainPage signUp(String username, String password) {
+        log.info("Try to sign up with '{}' username & '{}' password", username, password);
+
         driver.findElement(USERNAME_INPUT).sendKeys(username);
         driver.findElement(PASSWORD_INPUT).sendKeys(password);
         driver.findElement(SIGN_UP_SUBMIT_BUTTON).click();
@@ -40,6 +51,8 @@ public class MainPage {
     }
 
     public String getConfirmMessage() {
+        log.info("Get message from alert");
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.alertIsPresent());
         Alert alert = driver.switchTo().alert();
